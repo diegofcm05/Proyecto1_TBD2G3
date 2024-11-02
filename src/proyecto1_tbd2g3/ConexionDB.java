2738d87ecacc6a1c20be5f44cf514d2c80471629
Diego Cruz <dfcm950@gmail.com>
2024-11-01 19:34:33 -0600

    Commit Prueba Branch

---
+++ src/proyecto1_tbd2g3/ConexionDB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1_tbd2g3;
+import java.io.IOException;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class ConexionDB {
+    
+    private int num;
+    
+}
